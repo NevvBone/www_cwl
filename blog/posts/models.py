6d549467c1491c0c1667b0ebe655606aac68a6ab
Nikodem Gronowski <nikodemgronowski@gmail.com>
2025-12-01 19:24:17 +0100

    lab3

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,12 +1,46 @@
+from django.contrib.auth.models import User
 from django.db import models
-
+from django.utils import timezone
 
 class Category(models.Model):
     name = models.CharField(max_length=60)
-    description = models.TextField(default="",blank=True)
+    description = models.TextField(default='')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
 
 
 class Topic(models.Model):
     name = models.CharField(max_length=60)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'Topics'
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    text = models.TextField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    slug = models.SlugField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User,on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name_plural = 'Posts'
+
+    def __str__(self):
+        words = (self.text or "").split()
+        head = " ".join(words[:5])
+        return head + ("..." if len(words) > 5 else "")
